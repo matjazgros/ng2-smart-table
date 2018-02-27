7c3d6e54afa318d57f3198b3ccde601ee1cc6201
Matjaž Grosek <matjaz@grosek.si>
2018-02-27 16:01:20 +0100

    removed dist

--- dist/ng2-smart-table/lib/data-source/local/local.pager.js
+++
@@ -1,6 +0,0 @@
-export class LocalPager {
-    static paginate(data, page, perPage) {
-        return data.slice(perPage * (page - 1), perPage * page);
-    }
-}
-//# sourceMappingURL=local.pager.js.map
